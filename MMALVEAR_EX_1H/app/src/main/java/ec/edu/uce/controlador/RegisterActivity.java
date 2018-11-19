84547b90f2c2a8bcbcaa8839c14afbf9f63fc5bd
Monica <mmalvear@uce.edu.ec>
2018-11-19 16:16:33 -0500

    Examen-Monica-Alvear

---
+++ MMALVEAR_EX_1H/app/src/main/java/ec/edu/uce/controlador/RegisterActivity.java
@@ -0,0 +1,57 @@
+package ec.edu.uce.controlador;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import ec.edu.uce.R;
+import ec.edu.uce.componentes.CustomException;
+import ec.edu.uce.utilidades.Usuario;
+import ec.edu.uce.servicio.UserService;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private UserService userService = new UserService();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    public void register(View view) {
+        EditText txtUsername = findViewById(R.id.txtUsername);
+        EditText txtPassword = findViewById(R.id.txtPassword);
+
+        boolean guardar = true;
+        if (TextUtils.isEmpty(txtUsername.getText()) || TextUtils.isEmpty(txtPassword.getText())) {
+            guardar = false;
+            txtUsername.setError("Este campo es obligatorio");
+            txtPassword.setError("Este campo es obligatorio");
+        }
+
+        if (guardar) {
+            try {
+                userService.createUsersFileIfNotExist(this);
+
+                Usuario usuario = new Usuario(txtUsername.getText().toString(), txtPassword.getText().toString());
+                if (userService.existUsuario(usuario)) {
+                    Toast.makeText(this, "El nombre de usaurio " + usuario.getUsername() + " ya existe ingrese otro", Toast.LENGTH_LONG).show();
+                } else {
+                    userService.save(usuario);
+                    Toast.makeText(this, "Usuario " + usuario.getUsername() + " registrado correctamente", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+            } catch (CustomException e) {
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
